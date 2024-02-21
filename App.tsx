c9cd9fbb35189153f14b137de04fa3a9c91a0987
Carlos Alessandro <carllosalessandro@gmail.com>
2024-02-21 10:09:20 -0300

    feat: add styles css of component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import LoadingPage from './src/component/pages/loadingpage/LoadingPage';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import LoadingPage from "./src/component/pages/loadingpage/LoadingPage";
+import WelcomePage from "./src/component/pages/welcomePage/WelcomePage";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <LoadingPage />
+      {/* <LoadingPage /> */}
+      <WelcomePage />
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/component/pages/welcomePage/WelcomePage.tsx
@@ -0,0 +1,63 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import WelcomeArt from "../../../../assets/svg/welcome_page_art.svg";
+
+const WelcomePage = () => {
+  return (
+    <View style={styles.root}>
+      <Text style={styles.titleStyle}>Welcome to WhatsApp</Text>
+      <WelcomeArt />
+      <Text style={styles.descTextStyle}>
+        Read our{" "}
+        <Text style={styles.linkPrivacy} onPress={() => {}}>
+          Privacy Policy.
+        </Text>
+        Tap "Agree and continue" to accept the{" "}
+        <Text style={styles.linkPrivacy} onPress={() => {}}>
+          Teams of Service.
+        </Text>
+      </Text>
+      <Pressable onPress={() => {}} style={styles.buttonStyle}>
+        <Text style={styles.textButtonStyle}>agree and continue</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+export default WelcomePage;
+
+const styles = StyleSheet.create({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    width: "90%",
+    gap: 45,
+  },
+  titleStyle: {
+    fontFamily: "Roboto",
+    color: "#000",
+    fontSize: 25,
+    fontWeight: "500",
+  },
+  descTextStyle: {
+    fontFamily: "Roboto",
+    color: "#000",
+    fontSize: 15,
+    fontWeight: "400",
+    textAlign: "center",
+  },
+  linkPrivacy: {
+    color: "#4715FF",
+  },
+  buttonStyle: {
+    borderRadius: 10,
+    backgroundColor: "#086335",
+    width: "70%",
+  },
+  textButtonStyle: {
+    color: "#fff",
+    padding: 10,
+    textTransform: "uppercase",
+    textAlign: "center"
+  },
+});
