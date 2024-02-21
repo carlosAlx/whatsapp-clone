927a5c2e3bc754915206557f8c3ee24efe2a3bc7
Carlos Alessandro <carllosalessandro@gmail.com>
2024-02-21 09:15:52 -0300

    feat: add style css and image in the component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoadingPage from './src/component/pages/loadingpage/LoadingPage';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <LoadingPage />
       <StatusBar style="auto" />
     </View>
   );
--- src/component/pages/loadingpage/LoadingPage.tsx
+++ src/component/pages/loadingpage/LoadingPage.tsx
@@ -1,14 +1,42 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
+import BacgroundImg from "../../../../assets/svg/loading_background.svg";
+import LOGO from "../../../../assets/svg/logo_1.svg";
+
 const LoadingPage = () => {
   return (
     <View style={styles.root}>
-      <View></View>
+      <View>
+        <BacgroundImg />
+      </View>
+      <View style={styles.contentViewStyle}>
+        <LOGO width={70} height={70}/>
+        <Text style={styles.textStyle}>WhatsApp</Text>
+      </View>
     </View>
   );
 };
 
 export default LoadingPage;
 
-const styles = StyleSheet.create({ root: { position: "relative" } });
+const styles = StyleSheet.create({
+  root: { position: "relative" },
+  bgStyleView: { width: "100%" },
+  contentViewStyle: {
+    position: "absolute",
+    display: "flex",
+    flexDirection: "column",
+    gap: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+  },
+  textStyle:{
+    color: "#000",
+    fontSize: 35,
+    fontFamily: "Roboto",
+    fontWeight: "bold",
+  }
+});
