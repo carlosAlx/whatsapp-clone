47de5f78028fcee401dccb3f6d97cfa6763ee040
Carlos Alessandro <carllosalessandro@gmail.com>
2024-02-21 10:53:15 -0300

    add: add react-native-navigate

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoadingPage from "./src/component/pages/loadingpage/LoadingPage";
 import WelcomePage from "./src/component/pages/welcomePage/WelcomePage";
 
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <LoadingPage />    <WelcomePage /> */}
-      <LoadingPage /> 
-  
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{ headerShown: false, contentStyle: styles.container }}
+      >
+        <Stack.Screen name="welcomePage" component={WelcomePage} />
+        <Stack.Screen name="loadingPage" component={LoadingPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
